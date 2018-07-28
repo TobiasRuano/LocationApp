e07401034a56e93726e4b6b872620eec06ba1923
Tobias Ruano <ruano.t10@gmail.com>
2018-07-28 11:41:55 -0300

    Side menu added

--- LocationApp/ViewController.swift
+++ LocationApp/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let manager = CLLocationManager()
     
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var ButtonTraillingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sideMenuLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var ButtonleadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sideMenuView: UIView!
+    @IBOutlet weak var HambView: UIView!
+    var menuShowing = false
+    @IBOutlet weak var showMenu: UIButton!
+    
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var nameView: UIView!
     @IBOutlet weak var map: MKMapView!
@@ -21,7 +30,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var lugar = ""
     var longitud = CLLocationDegrees()
     var latitud = CLLocationDegrees()
+    var fetchedLocation = CLLocation()
     
+    @IBOutlet weak var settingsButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,8 +45,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
     @IBAction func updateLocation(_ sender: UIButton) {
+        //Taptic feedback
+        let generator = UIImpactFeedbackGenerator()
+        generator.impactOccurred()
+        
         setMap(longitud, latitud)
         locationLabel.text = lugar
+        lugar = ""
     }
     
     func style() {
@@ -43,10 +59,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         nameView.layer.shadowColor = UIColor.gray.cgColor
         nameView.layer.shadowOffset = CGSize(width: 0, height: 0)
         nameView.layer.shadowOpacity = 10
+        
         button.layer.cornerRadius = 8
         button.layer.shadowColor = UIColor.gray.cgColor
         button.layer.shadowOffset = CGSize(width: 0, height: 0)
         button.layer.shadowOpacity = 10
+        
+        HambView.layer.cornerRadius = 8
+        
+        sideMenuView.layer.cornerRadius = 8
+        sideMenuView.layer.shadowColor = UIColor.gray.cgColor
+        sideMenuView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        sideMenuView.layer.shadowOpacity = 10
+        
+        settingsButton.layer.cornerRadius = 8
+        
+        backgroundView.alpha = 0
     }
     
     func fetchCityAndCountry(location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?, _ error: Error?) -> ()) {
@@ -64,11 +92,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             guard let city = city, let country = country, error == nil else { return }
             //print(city + ", " + country)
             self.lugar = city + ", " + country
+            print(self.lugar)
         }
         
         self.longitud = location.coordinate.longitude
         self.latitud = location.coordinate.latitude
-        
+        fetchedLocation = location
     }
     
     func setMap(_ longitud: CLLocationDegrees, _ latitud: CLLocationDegrees) {
@@ -81,5 +110,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.map.showsUserLocation = true
     }
     
+
+    @IBAction func showMenu(_ sender: UIButton) {
+        
+        if (menuShowing) {
+            self.sideMenuLeadingConstraint.constant = -150
+            
+            UIView.animate(withDuration: 0.2) {
+                self.backgroundView.alpha = 0
+                self.view.layoutIfNeeded()
+            }
+        } else {
+            self.sideMenuLeadingConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.2) {
+                self.backgroundView.alpha = 0.6
+                self.view.layoutIfNeeded()
+            }
+        }
+        
+        menuShowing = !menuShowing
+        //Taptic feedback
+        let generator = UIImpactFeedbackGenerator()
+        generator.impactOccurred()
+    }
 }
 
